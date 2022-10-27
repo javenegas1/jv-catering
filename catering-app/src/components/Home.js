da020caa48bb7a1d512bc37cbd2a5b30563a8076
Juan <venegas.a.juan37@gmail.com>
2022-10-27 00:41:17 -0400

    nav and info card

--- catering-app/src/components/Home.js
+++ catering-app/src/components/Home.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+    <Card style={{ width: '20rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <Card.Body>
+        <Card.Title>Chef Juan Venegas</Card.Title>
+        <Card.Text>
+          With over 25 years of experience serving food to the people in and around Atlanta, I hope to have a plate of mine reach a table near you!
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Weddings, Events, Gatherings</ListGroup.Item>
+        <ListGroup.Item>Build your ideal menu</ListGroup.Item>
+        <ListGroup.Item>Pricing is negotiable</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="mailto:venegas.a.juan37@gmail.com">Send me an email to find out more!</Card.Link>
+      </Card.Body>
+    </Card>
+    </div>
   )
 }
--- catering-app/src/components/MainNav.js
+++ catering-app/src/components/MainNav.js
@@ -9,7 +9,7 @@ function MainNav() {
       <Navbar bg="dark" variant="dark" sticky="top">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="justify-content-end">
+          <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
