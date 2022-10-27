28d80fa3a943b008fa6994c970ba24f3b5804d90
Juan <venegas.a.juan37@gmail.com>
2022-10-27 00:15:57 -0400

    nav and home components

--- catering-app/src/App.js
+++ catering-app/src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import { Routes, Route, Outlet } from 'react-router-dom'
 
 import MainNav from './components/MainNav'
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <MainNav />
+      <div>
+        <MainNav />
+      </div>
       main comp
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- catering-app/src/components/MainNav.js
+++ catering-app/src/components/MainNav.js
@@ -6,17 +6,16 @@ import Navbar from 'react-bootstrap/Navbar';
 function MainNav() {
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark" sticky="top">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
+          <Nav className="justify-content-end">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-
     </div>
   )
 }
