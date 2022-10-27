9cecddc9500403fc00b4650c37710ddf5fec48b6
Juan <venegas.a.juan37@gmail.com>
2022-10-27 17:42:25 -0400

    homepage details added

--- catering-app/src/components/Home.js
+++ catering-app/src/components/Home.js
@@ -5,8 +5,14 @@ import ListGroup from 'react-bootstrap/ListGroup';
 export default function Home() {
   return (
     <div>
+        <div className='introduction' >
+            <div>
+                <h1>Gourmet Cooking</h1>
+                <p>ipsum</p>
+            </div>
+
     <Card style={{ width: '20rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <Card.Img variant="top" src="https://imgur.com/Y0Gd510.png" />
       <Card.Body>
         <Card.Title>Chef Juan Venegas</Card.Title>
         <Card.Text>
@@ -14,14 +20,15 @@ export default function Home() {
         </Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroup.Item>Weddings, Events, Gatherings</ListGroup.Item>
-        <ListGroup.Item>Build your ideal menu</ListGroup.Item>
+        <ListGroup.Item>A la Carte Catering</ListGroup.Item>
+        <ListGroup.Item>Meal Delivery to you</ListGroup.Item>
         <ListGroup.Item>Pricing is negotiable</ListGroup.Item>
       </ListGroup>
       <Card.Body>
-        <Card.Link href="mailto:venegas.a.juan37@gmail.com">Send me an email to find out more!</Card.Link>
+        <Card.Link href="mailto:juanjove31@yahoo.com">Send me an email to find out more!</Card.Link>
       </Card.Body>
     </Card>
+        </div>
     </div>
   )
 }
