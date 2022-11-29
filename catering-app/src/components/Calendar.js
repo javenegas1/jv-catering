0843c06c88a77863024f59b69d91e745ae570fa3
Juan <venegas.a.juan37@gmail.com>
2022-11-29 16:59:47 -0500

    calendar proofread

--- catering-app/src/components/Calendar.js
+++ catering-app/src/components/Calendar.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 export default function Calendar() {
   return (
-    <div>Calendar
+    <div>
+      <div>
+        <h3>Please feel free to see my upcoming events or contact me directly to work on scheduling yours!</h3>
+      </div>
 
       <div>
       <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=dmVuZWdhcy5hLmp1YW4zN0BnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=Y19jbGFzc3Jvb202NTY1OWI2Y0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%230B8043&color=%23202124" width="900" height="600" scrolling="no"></iframe>
